032dd00d466c8c83d0449b08343a832a0206f894
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-06-19 20:58:03 +0530

    regression done time to refactor

--- Benchmark.py
+++ Benchmark.py
@@ -1,19 +1,20 @@
-from sklearn.metrics import f1_score, classification_report
+from sklearn.metrics import mean_absolute_error, classification_report
+
 
 class Benchmark:
-    def __init__(self, cat: str = 'c', X = None, y = None, val_X = None, val_y = None, models: list[any] = None):
+    def __init__(self, cat: str = 'c', X=None, y=None, val_X=None, val_y=None, models: list[any] = None):
         self._X = X
         self._y = y
         self._val_X = val_X
         self._val_y = val_y
         self._models = models
         self._scores: dict = {}
-        
+
         if cat.lower() == 'c' or cat.lower() == 'r':
             self._cat = cat.lower()
         else:
             raise ValueError
-        #use c for classification and r for regression problems
+        # use c for classification and r for regression problems
 
     def calculate(self):
         self._scores = {}
@@ -22,7 +23,7 @@ class Benchmark:
         else:
             self._regression()
         return self._scores
-    
+
     def _classification(self):
         for i in self._models:
             i.fit(self._X, self._y)
@@ -30,4 +31,7 @@ class Benchmark:
             self._scores[i.__class__.__name__] = classification_report(self._val_y, pred)
 
     def _regression(self):
-        pass
+        for i in self._models:
+            i.fit(self._X, self._y)
+            pred = i.predict(self._val_X)
+            self._scores[i.__class__.__name__] = mean_absolute_error(self._val_y, pred)
