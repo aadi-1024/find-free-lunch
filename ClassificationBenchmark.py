f59c6fc0b112dd41af9b5ad60e050fe7af7aa47f
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-06-20 17:51:15 +0530

    fixes

--- ClassificationBenchmark.py
+++ ClassificationBenchmark.py
@@ -6,8 +6,8 @@ class ClassificationBenchmark(_BaseBenchmark):
     def __init__(self, X=None, y=None, val_X=None, val_y=None, models=None):
         super().__init__(X, y, val_X, val_y, models)
 
-    def benchmark(self, metric: callable = f1_score) -> dict:
+    def benchmark(self, metric: callable = f1_score, **kwargs) -> dict:
         for i in self._models:
-            self._scores[i.__class__.__name__] = metric(self._val_y, i.predict(self._val_X))
+            self._scores[i.__class__.__name__] = metric(self._val_y, i.predict(self._val_X), **kwargs)
 
         return self._scores
--- RegressionBenchmark.py
+++ RegressionBenchmark.py
@@ -6,8 +6,8 @@ class ClassificationBenchmark(_BaseBenchmark):
     def __init__(self, X=None, y=None, val_X=None, val_y=None, models=None):
         super().__init__(X, y, val_X, val_y, models)
 
-    def benchmark(self, metric: callable = mean_absolute_error) -> dict:
+    def benchmark(self, metric: callable = mean_absolute_error, **kwargs) -> dict:
         for i in self._models:
-            self._scores[i.__class__.__name__] = metric(self._val_y, i.predict(self._val_X))
+            self._scores[i.__class__.__name__] = metric(self._val_y, i.predict(self._val_X), **kwargs)
 
         return self._scores
