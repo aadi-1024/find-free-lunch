0b27761ec52cc245efd24daadb1f77a942dc4d83
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-06-20 20:28:50 +0530

    builds a package now, also changes

--- ffl/RegressionBenchmark.py
+++ ffl/RegressionBenchmark.py
@@ -1,12 +1,12 @@
-from BaseBenchmark import _BaseBenchmark
-from sklearn.metrics import f1_score
+from ffl.BaseBenchmark import _BaseBenchmark
+from sklearn.metrics import mean_absolute_error
 
 
 class ClassificationBenchmark(_BaseBenchmark):
     def __init__(self, X=None, y=None, val_X=None, val_y=None, models=None):
         super().__init__(X, y, val_X, val_y, models)
 
-    def benchmark(self, metric: callable = f1_score, **kwargs) -> dict:
+    def benchmark(self, metric: callable = mean_absolute_error, **kwargs) -> dict:
         for i in self._models:
             self._scores[i.__class__.__name__] = metric(self._val_y, i.predict(self._val_X), **kwargs)
 
